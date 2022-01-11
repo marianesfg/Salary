f795344ccde20255c33d4a21f6e2dde658cd6121
Mariane <mariane.sfgodoy@gmail.com>
2022-01-10 21:16:39 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Salary
+{
+    class Program
+    {
+        static void Main()
+        {
+            int cod = int.Parse(Console.ReadLine());
+
+            int horas = int.Parse(Console.ReadLine());
+            
+            decimal valor = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            decimal sal = horas * valor;
+
+            Console.WriteLine($"NUMBER = {cod}");
+            Console.WriteLine($"SALARY = {sal.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
